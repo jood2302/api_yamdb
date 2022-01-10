994dd978729e73ca00a50b4e8eed9388e6a4fbef
Evgen4567 <aes1993rich@gmail.com>
2022-01-10 20:49:37 +0500

    import / export for categories

--- api_yamdb/api/admin.py
+++ api_yamdb/api/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from .models import Categories, Genres, Titles
 
-admin.site.register(Categories)
+
+class CategoryResource(resources.ModelResource):
+    class Meta:
+        model = Categories
+        fields = ('id', 'name', 'slug',)
+
+
+class CategoriesAdmin(ImportExportModelAdmin):
+    resource_class = CategoryResource
+
+
+admin.site.register(Categories, CategoriesAdmin)
 admin.site.register(Genres)
 admin.site.register(Titles)
